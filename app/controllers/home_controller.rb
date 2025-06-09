8384dd41d33082fc0316c7dc8779afc28cf7b32f
Valentina Segato <v.segato@donboscosandona.it>
2025-06-09 15:56:44 +0200

    home_page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
+require 'ostruct'
 class HomeController < ApplicationController
     def index
+        
+
+        @carditems = [
+            OpenStruct.new(titolo: "[RBN] Casa Urbana", anno:"Interni", bottone:"scopri di più", immagine:"https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?cs=srgb&dl=pexels-fotoaibe-1571460.jpg&fm=jpg"),
+            OpenStruct.new(titolo: "Great Land, Allestimento Maxxi", anno:"Architettura", bottone:"scopri di più", immagine:"https://wallpapers.com/images/featured/architettura-4k-b3uwz18py1b0e5a7.jpg"),
+            
+        ]
     end
 end
